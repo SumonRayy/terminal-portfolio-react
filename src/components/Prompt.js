9ba3dbc68012f1b220e3a3522e488c685aab3885
SumonRayy <sumonrayy@gmail.com>
2021-07-18 11:24:38 +0530

    log removed

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -6,7 +6,6 @@ function Prompt({path}) {
         <div className='prompt'>
                 {myData.name}@<span className='prompt-user'>{myData.id}</span>:<span className='prompt-path'>/{path}</span> $ 
                 <span contentEditable="true" suppressContentEditableWarning={true} id='type' 
-                    onInput={e => console.log('typo : ', e.currentTarget.textContent)}
                 >type here...</span><span className='blink'>_</span>
                 
                 
